dc2f756d26fdbf645a32a24590b5dab337fcbb72
1998-felix <felix.gateru@gmail.com>
2021-05-26 18:58:11 +0300

    :sparkles: blink test sketch

--- Arduino-blink test/src/main.cpp
+++ Arduino-blink test/src/main.cpp
@@ -1,9 +1,19 @@
 #include <Arduino.h>
 
+#define LED_BUILTIN 13
+
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(LED_BUILTIN, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  Serial.println("Beginning");
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(50);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(50);
+
 }
